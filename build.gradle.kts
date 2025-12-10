e0b8102309c7dd2728c2069d652895f60feea3cc
Bababingo22 <bababingo22@gmail.com>
2025-12-10 16:30:24 +0300

    Fix Gradle Versions for Cloud Build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "8.2.1" apply false
     id("com.android.library") version "8.2.1" apply false
-    kotlin("android") version "1.9.0" apply false
+    kotlin("android") version "1.9.20" apply false
 }
 allprojects {
     repositories {
